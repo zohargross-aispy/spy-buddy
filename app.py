de395469133d440e812933fc95463253f26df124
zohargross-aispy <zohargross@gmail.com>
2026-03-29 11:12:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import yfinance as yf
-    from google import genai
-    import pandas as pd
+from google import genai
+import pandas as pd
     
     st.set_page_config(page_title="SPY Buddy PRO", page_icon="📈", layout="wide")
     st.title("📈 SPY Buddy Pro (Algo Edition)")
